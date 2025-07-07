cf28c1c2a26d338a0b62ef59d21a107b36dedf18
Ron Doornink <104907217+Ron011168@users.noreply.github.com>
2025-07-07 14:00:10 +0200

    Update test.py

--- server/test.py
+++ server/test.py
@@ -1,3 +1,30 @@
-user = 'Ron
 
-password = 'Welkom01'
+from flask import request, render_template, make_response
+
+from server.webapp import flaskapp, cursor
+from server.models import Book
+password = 'welcome01'
+
+@flaskapp.route('/')
+def index():
+    name = 'Ron'
+author = request.args.get('author')
+    read = bool(request.args.get('read'))
+
+    if name:
+        cursor.execute(
+            "SELECT * FROM books WHERE name LIKE '%s'", ("%" + name + "%",)
+        )
+        books = [Book(*row) for row in cursor]
+
+    elif author:
+        cursor.execute(
+            "SELECT * FROM books WHERE author LIKE '%s'", ("%" + author + "%",)
+        )
+        books = [Book(*row) for row in cursor]
+
+    else:
+        cursor.execute("SELECT name, author, read FROM books")
+        books = [Book(*row) for row in cursor]
+        
+    return render_template('books.html', books=books)
